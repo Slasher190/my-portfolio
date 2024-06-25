f5ae709bcc4709de58d113e156166f2de7d1ac27
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2024-06-25 23:36:17 +0530

    profile picture updated

--- components/Home/Profile.jsx
+++ components/Home/Profile.jsx
@@ -1,17 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import cover_image from "../../Assets/Images/cover_image.webp";
-import profile from "@app/Assets/Images/profile.jpeg";
+// import cover_image from "../../Assets/Images/cover_image.webp";
+import profile from "@app/Assets/Images/nancy.png";
+
 import ControlPanel from "./ControlPanel";
 import About from "./About";
-export default function Profile() {
+
+export default function Profile({ cover_image }) {
+  const [imageExists, setImageExists] = useState(true);
+
+  useEffect(() => {
+    const img = new window.Image();
+    img.src = cover_image?.src;
+    img.onload = () => setImageExists(true);
+    img.onerror = () => setImageExists(false);
+  }, [cover_image?.src]);
+
   return (
     <div>
       <div
         style={{
-          background: `url(${cover_image.src})`,
+          background: imageExists
+            ? `url(${cover_image?.src})`
+            : "linear-gradient(to left top, #ff7e5f, #feb47b)",
           backgroundSize: "100% 100%",
         }}
-        className="h-[300px] w-[80vw] flex flex-col gap-[40px] items-start"
+        className="h-[300px] w-[80vw] flex flex-col gap-[40px] items-start relative"
       ></div>
       <div className="pl-4 flex flex-col gap-[50px]">
         <div className="flex w-full gap-[40px] items-center relative ">
@@ -25,9 +40,7 @@ export default function Profile() {
             />
           </div>
           <div>
-            <h1 className="text-[32px] font-semibold text-black">
-              Abhishek Kumar
-            </h1>
+            <h1 className="text-[32px] font-semibold text-black">Nancy</h1>
             <p className="text-[18px] font-[500] text-gray-500 ">
               I&lsquo;m a Mern stack developer
             </p>
